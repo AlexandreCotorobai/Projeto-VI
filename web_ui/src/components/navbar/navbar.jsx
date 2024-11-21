0eacdc00e9bb9dd67d444a26d8fbc0c847215e9f
André Oliveira <andreaoliveira@ua.pt>
2024-11-21 14:22:39 +0000

    Update navbar.jsx

--- web_ui/src/components/navbar/navbar.jsx
+++ web_ui/src/components/navbar/navbar.jsx
@@ -2,22 +2,33 @@ import { Link } from "react-router-dom";
 
 export const Navbar = (props) => {
   return (
-    <div className="navbar bg-blue-500 rounded fixed z-[1]">
-      <div className="flex-1 flex justify-between items-center text-white">
-        <div className="flex space-x-10">
-          <Link to="/" className="text-xl ml-8">
+    <div className="navbar bg-[#345d7e] rounded fixed z-[1] shadow-lg">
+      <div className="flex-1 flex justify-between items-center text-white px-4 md:px-10">
+        <div className="flex space-x-4 md:space-x-10">
+          <Link
+            to="/"
+            className="text-xl hover:text-[#e63946] transition-colors duration-300"
+          >
             Inovação
           </Link>
-          <Link to="/invest" className="text-xl">
+          <Link
+            to="/invest"
+            className="text-xl hover:text-[#e63946] transition-colors duration-300"
+          >
             Investimento
           </Link>
-          <Link to="/infra" className="text-xl">
+          <Link
+            to="/infra"
+            className="text-xl hover:text-[#e63946] transition-colors duration-300"
+          >
             Infraestrutura
           </Link>
         </div>
-        <div className="normal-case text-xl mr-10">
-          <p>🇨🇳China-🇯🇵Japan</p>
-          <p>Guerra Tecnológica</p>
+        <div className="text-center normal-case text-lg md:text-xl">
+          <p className="font-bold text-white">🇨🇳 China - 🇯🇵 Japan</p>
+          <p className="text-sm md:text-base text-[#ffffff99]">
+            Guerra Tecnológica
+          </p>
         </div>
       </div>
     </div>
