060df0cdc5c9fd8749337029e5371de31813d63a
André Oliveira <andreaoliveira@ua.pt>
2024-11-24 23:25:03 +0000

    coisas

--- web_ui/tailwind.config.js
+++ web_ui/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [daisyui],
 };
